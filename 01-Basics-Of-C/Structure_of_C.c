228ba75c717f67525f6054559400230624fbfc70
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-22 19:26:24 +0530

    Structure of a C programming

---
+++ 01-Basics-Of-C/Structure_of_C.c
@@ -0,0 +1,14 @@
+// First one is the identify the problem Comment it........
+
+// include the header file
+#include<stdio.h>
+
+// start the main function
+int main()   // datatype main() { ...... }
+{
+    // open the bracket start to end
+
+// End add the Return Values
+    return 0;
+}
+
